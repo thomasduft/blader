9398a7346fdc77de767739722b23aedc3f947686
Thomas Duft <thomas.duft@gmail.com>
2017-08-16 20:54:46 +0200

    updated latest angular packages

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,31 +1,6 @@
 import 'rxjs/Rx';
 
-// import { SystemJsNgModuleLoader, ApplicationInitStatus } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
-
 platformBrowserDynamic().bootstrapModule(AppModule);
-
-/*
-  .then(appModule => {
-  modules.map(module => bootstrapComponent(module, appModule));
-});
-
-const modules = [
-  'app/lazy/lazy.module#LazyModule'
-];
-
-function bootstrapComponent(module: string, appModule: any) {
-  const loader = appModule.injector.get(SystemJsNgModuleLoader);
-  return loader.load(module)
-    .then((factory: any) => {
-      const ngModule = factory.create(appModule.injector);
-      let initStatus = ngModule.injector.get(ApplicationInitStatus);
-
-      initStatus.donePromise.then(() => {
-        console.log(`${module} is initialized...`);
-      });
-    });
-}
-*/
